1a90cfc8d032e4588f23d7fe960d83966ed08994
Zohaib Khan <mr.zohaib@live.com>
2023-10-06 02:35:15 +0500

    migration files included

---
+++ src/services/Shipment.Infrastructure/Migrations/20231005213001_RemoveFaxColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shipment.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFaxColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fax",
+                table: "Address");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Fax",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
